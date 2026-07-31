65216fef9e84ebef189ded72fc435bc8ca3e3b8c
Jemma Little <Jlittle2@uccs.edu>
2026-07-31 14:07:47 -0600

    Made head of snake an object. Created a class GameObject

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
+from game_object import GameObject
 import pygame
 
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Snake")
+snake_head = GameObject(100,100,50,50,(255,255,255))
 
 clock = pygame.time.Clock()
 pygame.time.wait(1600)
+
 
 running = True
 while running:
@@ -16,7 +19,7 @@ while running:
             running = False
 #UPDATE
     screen.fill((0,0,0))
-    pygame.draw.rect(screen,(255,255,255),(100,100,50,50))
+    snake_head.draw(screen)
 
 #DRAW    
     pygame.display.flip()
