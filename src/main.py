74cafe841366b98dfaf36b6ed4208d0621ab2eb4
Jemma Little <Jlittle2@uccs.edu>
2026-07-31 12:37:15 -0600

    Added a title

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Snake")
 
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
